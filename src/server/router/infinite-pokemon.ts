ecd9631514a8b1bd56b8111f7d6a59eb4d66240d
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-17 18:54:06 +0200

    add firstPos param to infinite pokemon query

--- src/server/router/infinite-pokemon.ts
+++ src/server/router/infinite-pokemon.ts
@@ -7,6 +7,7 @@ const ONE_DAY_MILISECONDS = 60 * 60 * 24 * 1000;
 export const infinitePokemonRouter = createRouter().query("get-infinite-pokemon", {
     input: z.object({
         limit: z.number().min(1).max(50).nullish(),
+        firstPos: z.number().nullish(),
         cursor: z.number().nullish(),
     }),
     async resolve({ input }) {
@@ -14,11 +15,12 @@ export const infinitePokemonRouter = createRouter().query("get-infinite-pokemon"
             cacheOptions: { maxAge: ONE_DAY_MILISECONDS, exclude: { query: false } },
         });
         const limit = input.limit ?? 50;
+        const firstPos = input.firstPos ? input.firstPos : 0;
 
         const { cursor } = input;
 
         let offset = cursor ? cursor : 0;
-        const pokemons = await api.listPokemons(offset, limit);
+        const pokemons = await api.listPokemons(firstPos + offset, limit);
 
         let nextCursor: typeof cursor | null = input.cursor;
 
