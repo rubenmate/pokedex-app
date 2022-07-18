4f073e1e656de478289d1c85b33f2279687e0ea7
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-19 00:03:35 +0200

    feat: add mobile responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { ScaleLoader } from "react-spinners";
 import { MdModeNight } from "react-icons/md";
 import { BsSun } from "react-icons/bs";
 import { FiGithub } from "react-icons/fi";
+import { FiChevronDown } from "react-icons/fi";
 
 const LIMIT = 27;
 
@@ -59,154 +60,105 @@ const Home: NextPage = () => {
                 <meta name="description" content="Pokedex App" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="flex max-w-6xl mx-auto justify-between items-center">
-                <div className="flex items-center">
-                    <h2 className="ml-2 text-xl md:text-3xl font-extrabold text-gray-700 dark:text-white">
-                        Pokedex App
-                    </h2>
-                    <div className="ml-4">
-                        <Menu>
-                            <Menu.Button
-                                className={
-                                    "font-bold bg-gray-100 rounded-sm shadow p-2 dark:text-black"
-                                }
+            <div className="dark:bg-grayish">
+                <div className="flex max-w-6xl mx-auto justify-between items-center">
+                    <div className="flex items-center">
+                        <h2 className="ml-2 text-xl sm:text-3xl font-extrabold text-gray-700 dark:text-white">
+                            PokeApp
+                        </h2>
+                        <div className="ml-2 sm:ml-4">
+                            <Menu
+                                as="div"
+                                className="relative inline-block text-left dark:text-black"
                             >
-                                Start from:
-                            </Menu.Button>
-                            <Menu.Items>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(0)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            1st gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(151)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            2nd gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(251)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            3rd gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(386)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            4th gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(493)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            5th gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(649)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            6th gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(721)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            7th gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(809)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            8th gen
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => setFirstPos(898)}
-                                            className="ml-2 p-2 bg-blue-300 rounded-md text-black"
-                                        >
-                                            Others
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                            </Menu.Items>
-                        </Menu>
+                                <Menu.Button
+                                    className="inline-flex w-full justify-center rounded-md 
+                                bg-white border-black border px-1 sm:px-4 py-2 text-sm font-medium 
+                                hover:bg-gray-100 group"
+                                >
+                                    Start from
+                                    <FiChevronDown
+                                        className="ml-1 sm:ml-2 -mr-1 h-5 w-5 text-violet-800 
+                                    group-hover:text-violet-500"
+                                    />
+                                </Menu.Button>
+                                <Menu.Items className="absolute flex flex-col">
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                className="py-2 mt-1 w-48 border border-black 
+                                                bg-white rounded-md"
+                                                onClick={() => setFirstPos(0)}
+                                            >
+                                                1st gen
+                                            </button>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                className="py-2 w-48 border border-black bg-white 
+                                                rounded-md"
+                                                onClick={() => setFirstPos(151)}
+                                            >
+                                                2nd gen
+                                            </button>
+                                        )}
+                                    </Menu.Item>
+                                </Menu.Items>
+                            </Menu>
+                        </div>
+                    </div>
+                    <div className="justify-self-end flex items-center">
+                        <a
+                            href="https://github.com/rubenmate/pokedex-app"
+                            className="hidden md:flex items-center mr-2 bg-gray-100 pr-1 rounded-md 
+                        font-semibold dark:text-black hover:underline shadow"
+                        >
+                            <div className="mr-2 p-2 bg-gray-600 rounded-sm">
+                                <FiGithub className="text-white" />
+                            </div>
+                            GitHub
+                        </a>
+                        <a
+                            href="https://github.com/rubenmate/pokedex-app"
+                            className="md:hidden items-center dark:text-black hover:underline shadow"
+                        >
+                            <div className="p-3 bg-gray-600 rounded-md">
+                                <FiGithub className="text-white" />
+                            </div>
+                        </a>
+                        <button
+                            className="m-2 w-10 h-10 rounded-md dark:bg-yellow-400 bg-blue-400 
+                        dark:text-black text-white flex justify-center items-center shadow"
+                            onClick={() => {
+                                setTheme(theme === "dark" ? "light" : "dark");
+                            }}
+                        >
+                            {theme === "dark" ? (
+                                <BsSun className="w-5 h-5" />
+                            ) : (
+                                <MdModeNight className="w-5 h-5" />
+                            )}
+                        </button>
                     </div>
                 </div>
-                <div className="justify-self-end flex items-center">
-                    <a
-                        href="https://github.com/rubenmate/pokedex-app"
-                        className="flex items-center mr-2 bg-gray-100 pr-1 rounded-md font-semibold dark:text-black hover:underline shadow"
-                    >
-                        <div className="mr-2 p-2 bg-gray-600 rounded-sm">
-                            <FiGithub className="text-white" />
-                        </div>
-                        GitHub
-                    </a>
-                    <button
-                        className="m-2 w-10 h-10 rounded-md dark:bg-yellow-400 bg-blue-400 dark:text-black text-white flex justify-center items-center shadow"
-                        onClick={() => {
-                            setTheme(theme === "dark" ? "light" : "dark");
-                        }}
-                    >
-                        {theme === "dark" ? (
-                            <BsSun className="w-5 h-5" />
-                        ) : (
-                            <MdModeNight className="w-5 h-5" />
-                        )}
-                    </button>
-                </div>
-            </div>
-            <div className="flex flex-col justify-center items-center p-4 dark:bg-grayish">
-                <div
-                    className="grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 
+                <div className="flex flex-col justify-center items-center p-4">
+                    <div
+                        className="grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 
                 lg:grid-cols-3 md:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full"
-                >
-                    {data?.pages.map((page) =>
-                        page.pokemons.results.map((element) => {
-                            let id = Number(extractPokemonIdFromURL(element.url));
-                            return <PokemonCard key={id} id={id} name={element.name} />;
-                        })
-                    )}
-                    {hasNextPage && <div ref={divToLoadPokemonsRef} />}
-                </div>
-                <div className="btn-container">
-                    <Toaster position="bottom-right" reverseOrder={false} />
+                    >
+                        {data?.pages.map((page) =>
+                            page.pokemons.results.map((element) => {
+                                let id = Number(extractPokemonIdFromURL(element.url));
+                                return <PokemonCard key={id} id={id} name={element.name} />;
+                            })
+                        )}
+                        {hasNextPage && <div ref={divToLoadPokemonsRef} />}
+                    </div>
+                    <div className="btn-container">
+                        <Toaster position="bottom-right" reverseOrder={false} />
+                    </div>
                 </div>
             </div>
         </>
